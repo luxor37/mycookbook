27d6052aacac78e399a53904dbdc80ac78c052dd
Remi Martel <luxor37@gmail.com>
2023-11-18 14:34:20 -0500

    cleanup & css

--- pages/index.vue
+++ pages/index.vue
@@ -5,8 +5,7 @@ definePageMeta({
 import { useRecipeStore } from "~/stores/recipes";
 import Recipe, { CATEGORY } from "~/types/recipe";
 
-const router = useRouter();
-const routeType = ref<CATEGORY>("ALL");
+const routeType = ref<CATEGORY>(CATEGORY.ALL);
 const recipeStore = useRecipeStore();
 
 const filteredRecipes = computed((): Recipe[] => {
@@ -24,58 +23,12 @@ const filteredRecipes = computed((): Recipe[] => {
     class="flex flex-wrap m-2 md:mx-52"
     v-if="recipeStore.recipes !== null && filteredRecipes.length > 0"
   >
-    <div
-      @click="router.push(`/recette?id=${id}`)"
+    <RecipeCard
       v-for="{ id, title, image } in filteredRecipes"
       :key="id"
-      class="w-1/2 max-w-xs cursor-pointer p-2"
-    >
-      <div class="aspect-ratio">
-        <div
-          class="aspect-content mt-4 rounded-lg bg-slate-500 bg-zoom"
-          :style="`background-image: url(${image});`"
-        >
-          <div class="h-full w-full bg-gradient rounded-lg">
-            <div class="text-white bottom-0 absolute w-full p-2">
-              {{ title }}
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+      :id="id"
+      :title="title"
+      :image="image"
+    />
   </div>
 </template>
-
-<style scoped>
-.aspect-ratio {
-  width: 100%; /* or any width you desire */
-  padding-top: 100%; /* 1:1 Aspect Ratio */
-  position: relative; /* If you want text inside of the container */
-}
-
-.aspect-content {
-  position: absolute;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
-}
-
-.bg-zoom {
-  background-size: cover;
-  transition: background-size 0.5s ease-in-out;
-  background-position: center;
-}
-
-.bg-zoom:hover {
-  background-size: 110%;
-}
-
-.bg-gradient {
-  background: linear-gradient(
-    to top,
-    rgba(0, 0, 0, 0.7) 0%,
-    rgba(0, 0, 0, 0) 60%
-  );
-}
-</style>
